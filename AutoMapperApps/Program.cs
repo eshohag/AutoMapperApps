01c09df4a737e6812fae3e45e4f9a0ea1ed71154
mdshohagmia <shohaghassan14@hotmail.com>
2019-03-28 12:39:29 +0600

    Initial

--- AutoMapperApps/Program.cs
+++ AutoMapperApps/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoMapperApps
 {
@@ -10,6 +7,41 @@ namespace AutoMapperApps
     {
         static void Main(string[] args)
         {
+            //var student = new Student()
+            //{
+            //    Id = 1,
+            //    FirstName = "Shohag",
+            //    LastName = "Mia",
+            //    ContactNo = "01926029000",
+            //    Email = "shohaghassan14@gmail.com"
+            //};
+            //AutoMapper.Mapper.Initialize(configureModelSourceToDestination =>
+            //{
+            //    configureModelSourceToDestination.CreateMap<Student, Teacher>()
+            //        .ForMember(destination => destination.FullName, opt => opt.MapFrom(a => a.FirstName));
+            //});
+            //var teacher = AutoMapper.Mapper.Map<Teacher>(student);
+
+            List<Student> students = new List<Student>()
+            {
+                new Student(){Id=1,FirstName = "Shohag",LastName = "Mia",ContactNo = "01926029000",Email = "shohaghassan14@gmail.com"},
+                new Student(){Id=1,FirstName = "Shohag",LastName = "Mia",ContactNo = "01926029000",Email = "shohaghassan14@gmail.com"},
+                new Student(){Id=1,FirstName = "Shohag",LastName = "Mia",ContactNo = "01926029000",Email = "shohaghassan14@gmail.com"},
+                new Student(){Id=1,FirstName = "Shohag",LastName = "Mia",ContactNo = "01926029000",Email = "shohaghassan14@gmail.com"},
+                new Student(){Id=1,FirstName = "Shohag",LastName = "Mia",ContactNo = "01926029000",Email = "shohaghassan14@gmail.com"},
+            };
+            AutoMapper.Mapper.Initialize(configureModelSourceToDestination =>
+            {
+                configureModelSourceToDestination.CreateMap<Student, Teacher>();
+            });
+
+            var teachers = AutoMapper.Mapper.Map<List<Teacher>>(students);
+            foreach (Teacher aTeacher in teachers)
+            {
+                Console.WriteLine($"Id-{aTeacher.Id}, Full Name-{aTeacher.FirstName} {aTeacher.LastName}, Contact No-{aTeacher.ContactNo} & Email-{aTeacher.Email}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ AutoMapperApps/Teacher.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoMapperApps
+{
+    class Teacher
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+    }
+}
